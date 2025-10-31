caf193b7722292d542b98a6267b91a2ddd00cac2
Johnathan Falk <johnathan.falk@gmail.com>
2025-10-31 19:31:54 -0400

    fix: update comment syntax in Node.js test fixture files
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- testdata/node/index.js
+++ testdata/node/index.js
@@ -1,5 +1,5 @@
-# file: testdata/node/index.js
-# version: 1.0.0
-# guid: 13579bdf-2468-ace0-b135-79bdf13579bd
+// file: testdata/node/index.js
+// version: 1.0.0
+// guid: 13579bdf-2468-ace0-b135-79bdf13579bd
 
 console.log("✅ Node.js test fixture executed successfully.");
